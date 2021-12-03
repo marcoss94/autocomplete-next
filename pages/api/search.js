b0080150b3f55e0575c928d268f86e4dda4fad47
marcos <marcos.alvarez@tolber.io>
2021-12-03 12:08:48 -0300

    load-project

---
+++ pages/api/search.js
@@ -0,0 +1,22 @@
+import db from '../../db.json'
+
+export default function handler(req, res) {
+    const { id, q } = req.query
+
+    // we have an id available
+    if (id) {
+        const item = db.find((item) => item.id === +id)
+        return res.status(200).json(item)
+    }
+
+    // we have a keyword to search for
+    if (q) {
+        const results = db.filter(({ title }) => {
+            return title.toLowerCase().includes(q.toLowerCase())
+        })
+        return res.status(200).json(results)
+    }
+
+    // we don't have anything
+    res.status(400).json()
+}
--- pages/index.js
+++ pages/index.js
@@ -1,28 +1,30 @@
+import Search from '../components/Search.jsx'
 import Head from 'next/head'
+import Image from 'next/image'
+
+import Footer from '../components/Footer.jsx'
+import MainLogo from '../components/MainLogo.jsx'
+import HomeResults from '../components/HomeResults.jsx'
 
 export default function Home() {
     return (
-        <div className='flex flex-col items-center justify-center min-h-screen py-2'>
+        <div>
             <Head>
-                <title>Create Next App</title>
+                <title>¡Black Friday Autocomplete App!</title>
+                <meta
+                    name='description'
+                    content='Black Friday Autocomplete App'
+                />
                 <link rel='icon' href='/favicon.ico' />
             </Head>
 
-            <main className='flex flex-col items-center justify-center w-full flex-1 px-20 text-center'>
-                <h1 className='text-6xl font-bold'>
-                    Auto Complete{' '}
-                    <a className='text-blue-600' href='https://nextjs.org'>
-                        Next.js!
-                    </a>
-                </h1>
-
-                <p className='mt-3 text-2xl'>
-                    Get started by editing{' '}
-                    <code className='p-3 font-mono text-lg bg-gray-100 rounded-md'>
-                        pages/index.js
-                    </code>
-                </p>
+            <main>
+                <MainLogo />
+                <Search />
+                <HomeResults />
             </main>
+
+            <Footer />
         </div>
     )
 }
